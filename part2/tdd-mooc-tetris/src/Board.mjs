971a6d009abf5dd08c0abed32b6886cd18a4072d
Elias Herranen <elias.herranen@gmail.com>
2023-01-08 12:01:33 +0200

    Added tick method to board.mjs, made 4th test to pass

--- part2/tdd-mooc-tetris/src/Board.mjs
+++ part2/tdd-mooc-tetris/src/Board.mjs
@@ -11,7 +11,28 @@ export class Board {
 
   drop(block) {
     const middle = Math.floor(this.width / 2);
-    this.gameboard[0][middle] = "X";
+    this.gameboard[0][middle] = block.color;
+  }
+
+  tick() {
+    for (let row = 0; row < this.gameboard.length; row++) {
+      for (let column = 0; column < this.gameboard[row].length; column++) {
+        if (this.hasBlock(row, column)) {
+          this.moveBlockDownByOne(row, column);
+          return;
+        }
+      }
+    }
+  }
+
+  hasBlock(row, column) {
+    return this.gameboard[row][column] !== ".";
+  }
+
+  moveBlockDownByOne(row, column) {
+    const blockToMove = this.gameboard[row][column];
+    this.gameboard[row][column] = ".";
+    this.gameboard[row + 1][column] = blockToMove;
   }
 
   toString() {
