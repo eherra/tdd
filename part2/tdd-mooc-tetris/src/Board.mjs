e13fd23ea1bc7cb547b8e4c7507b1d793e4ade50
Elias Herranen <elias.herranen@gmail.com>
2023-01-08 11:47:57 +0200

    Created drop method for Board.mjs, made 2nd test to pass

--- part2/tdd-mooc-tetris/src/Board.mjs
+++ part2/tdd-mooc-tetris/src/Board.mjs
@@ -1,17 +1,24 @@
 export class Board {
   width;
   height;
+  gameboard;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.gameboard = Array(height).fill().map(() => Array(width).fill("."));
+  }
+
+  drop(block) {
+    const middle = Math.floor(this.width / 2);
+    this.gameboard[0][middle] = "X";
   }
 
   toString() {
     let shape = "";
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        shape += ".";
+    for (let row = 0; row < this.gameboard.length; row++) {
+      for (let column = 0; column < this.gameboard[row].length; column++) {
+        shape += this.gameboard[row][column];
       }
       shape += "\n";
     }
